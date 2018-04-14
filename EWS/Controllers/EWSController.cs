25f4a7b0f37db7c8c39c89fc9d8b70853964dc3b
coolchungho <coolchungho@gmail.com>
2018-04-14 23:18:23 +0800

    add EWS

---
+++ EWS/Controllers/EWSController.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using EWS.Models;
+
+namespace EWS.Controllers
+{
+    public class EWSController : Controller
+    {
+        // GET: EWS
+        public ActionResult Index([FromForm] EWSModel ewsmodel)
+        {
+            int RR = ewsmodel.RR;
+            int RR_score = 0;
+            if (RR < 9 || RR > 35)
+            { RR_score = 3; }
+            else if (RR >= 31 && RR <= 35)
+            { RR_score = 2; }
+            else if (RR >= 21 && RR <= 30)
+            { RR_score = 1; }
+            else { RR_score = 0; }
+
+
+            int OxySat = ewsmodel.OxySat;
+            int OxySat_score = 1;
+            var SuppOxy = ewsmodel.SuppOxy;
+            int SuppOxy_score = 2;
+            var Temp = ewsmodel.Temp;
+            int Temp_score = 0;
+            int SysBP = ewsmodel.SysBP;
+            int SysBP_score = 1;
+            int HR = ewsmodel.HR;
+            int HR_score = 1;
+            var AVPU = ewsmodel.AVPU;
+            int AVPU_score = 0;
+            int EWS = RR_score + OxySat_score + SuppOxy_score + Temp_score + SysBP_score + HR_score + AVPU_score;
+
+            ;
+            ViewBag.RR = RR;
+            ViewBag.RR_score = RR_score;
+            ViewBag.OxySat = OxySat;
+            ViewBag.SuppOxy = SuppOxy;
+            ViewBag.Temp = Temp;
+            ViewBag.SysBP = SysBP;
+            ViewBag.HR = HR;
+            ViewBag.AVPU = AVPU;
+            ViewBag.ews = EWS;
+
+            return View();
+        }
+
+        // GET: EWS/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: EWS/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: EWS/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: EWS/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: EWS/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: EWS/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: EWS/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ EWS/Models/EWSModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EWS.Models
+{
+    public class EWSModel
+    {
+        public int RR { get; set; }
+        public int OxySat { get; set; }
+        public bool SuppOxy { get; set; }
+        public decimal Temp { get; set; }
+        public int SysBP { get; set; }
+        public int HR { get; set; }
+        public string AVPU { get; set; }
+        public int RR_score { get; set; }
+        public int OxySat_score { get; set; }
+        public int SuppOxy_score { get; set; }
+        public int Temp_score { get; set; }
+        public int SysBP_score { get; set; }
+        public int HR_score { get; set; }
+        public int AVPU_score { get; set; }
+        public int EWS { get; set; }
+    }
+}
